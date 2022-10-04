1131abd1ce4bd31253641b65e11a1d2500d4e313
NestorMartire <nestorm1979@hotmail.com>
2022-10-04 11:35:47 -0300

    first commit

---
+++ JS_ejercicio5.js
@@ -0,0 +1,11 @@
+let miAltura = 178; //- Una variable que contenga tu altura en centímetros (entero)
+let miAlturaFloat = 1.78 //- Una variable que contenga tu altura en metros (número de coma flotante)
+let miPeso = 85.6 //- Una variable que contenga tu peso en kilogramos (número de coma flotante)
+miAlturaFloatRed = Math.ceil(miAlturaFloat)
+console.log(miAlturaFloatRed) //- Una variable que contenga tu altura en metros redondeada hacia arriba
+miPesoBajado=Math.floor(miPeso);
+console.log(miPesoBajado) //- Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+let x = Number.MAX_VALUE;
+let y = x + 1;
+z = (x === y);
+console.log(z); //- Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
